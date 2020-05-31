2706adad59e3de35a10dc9ac5e546f1a2f0afa1a
ThisNils <ThisNils@github.com>
2020-05-31 22:12:33 +0200

    Fixed Cannot GeT /exchange issue

--- index.js
+++ index.js
@@ -14,7 +14,52 @@ class EpicGamesClientLoginAdapter {
   async getExchangeCode () {
     try {
       const page = await this.browser.pages().then(pages => pages[0]);
-      const response = await page.goto('https://www.epicgames.com/id/api/exchange').then(response => response.json());
+
+      const oldXsrfToken = (await page.cookies()).find((c) => c.name === 'XSRF-TOKEN').value;
+      page.once('request', (req) => {
+        req.continue({
+          method: 'GET',
+          headers: {
+            ...req.headers,
+            'X-XSRF-TOKEN': oldXsrfToken,
+          },
+        });
+      });
+      await page.setRequestInterception(true);
+      await page.goto('https://www.epicgames.com/id/api/authenticate');
+      await page.setRequestInterception(false);
+      
+      page.once('request', (req) => {
+        req.continue({
+          method: 'GET',
+          headers: {
+            ...req.headers,
+            'X-XSRF-TOKEN': oldXsrfToken,
+          },
+        });
+      });
+      await page.setRequestInterception(true);
+        try {
+          await page.goto('https://www.epicgames.com/id/api/csrf');
+        } catch (e) {}
+      await page.setRequestInterception(false);
+      
+
+      const xsrfToken = (await page.cookies()).find((c) => c.name === 'XSRF-TOKEN').value;
+      page.once('request', (req) => {
+        req.continue({
+          method: 'POST',
+          headers: {
+            ...req.headers,
+            'X-XSRF-TOKEN': xsrfToken,
+          },
+        });
+      });
+      await page.setRequestInterception(true);
+      const response = await (await page.goto('https://www.epicgames.com/id/api/exchange/generate')).json();
+      await page.setRequestInterception(false);
+
+
       if (!response.code) {
         throw new ExchangeCodeException(`Unexcepted response: ${JSON.stringify(response)}`);
       }
@@ -53,14 +98,16 @@ class EpicGamesClientLoginAdapter {
     await page.goto('https://epicgames.com/id');
     const login = credentials.login || credentials.email || credentials.username;
     if (login && credentials.password) {
-      const usernameOrEmailField = await page.waitForSelector('#usernameOrEmail');
+      const loginWithEpicButton = await page.waitForSelector('#login-with-epic');
+      await loginWithEpicButton.click();
+      const usernameOrEmailField = await page.waitForSelector('#email');
       await usernameOrEmailField.type(login, { delay: options.inputDelay });
       const passwordField = await page.waitForSelector('#password');
       await passwordField.type(credentials.password, { delay: options.inputDelay });
       const loginButton = await page.waitForSelector('#login:not(:disabled)');
       await loginButton.click();
     }
-    await page.waitForRequest(request => request.url() === 'https://www.epicgames.com/account/personal' && request.method() === 'GET', {
+    await page.waitForResponse((response) => response.url() === 'https://www.epicgames.com/account/personal', {
       timeout: options.enterCredentialsTimeout,
     });
     return new this(browser);
