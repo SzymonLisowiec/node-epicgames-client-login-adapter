5e4fd3ffccf95dbc2f848fe864ca43c840d1e771
Dāvis <davispuh@gmail.com>
2020-08-25 22:59:23 +0300

    Handle case when we're already logged in based on given cookies

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const Puppeteer = require('puppeteer');
+const { PendingXHR } = require('pending-xhr-puppeteer');
 const ExchangeCodeException = require('./exceptions/ExchangeCodeException');
 
 class EpicGamesClientLoginAdapter {
+
+  static get ACCOUNT_PAGE() {
+    return 'https://www.epicgames.com/account/personal';
+  }
 
   constructor (browser) {
     this.browser = browser;
@@ -19,35 +24,39 @@ class EpicGamesClientLoginAdapter {
     try {
       const page = await this.getPage();
 
-      const oldXsrfToken = (await page.cookies()).find((c) => c.name === 'XSRF-TOKEN').value;
-      page.once('request', (req) => {
-        req.continue({
-          method: 'GET',
-          headers: {
-            ...req.headers,
-            'X-XSRF-TOKEN': oldXsrfToken,
-          },
+      const oldXsrfToken = (await page.cookies()).find((c) => c.name === 'XSRF-TOKEN');
+      if (oldXsrfToken) {
+        page.once('request', (req) => {
+          req.continue({
+            method: 'GET',
+            headers: {
+              ...req.headers,
+              'X-XSRF-TOKEN': oldXsrfToken.value,
+            },
+          });
         });
-      });
-      await page.setRequestInterception(true);
+      }
+      await page.setRequestInterception(Boolean(oldXsrfToken));
       await page.goto('https://www.epicgames.com/id/api/authenticate');
       await page.setRequestInterception(false);
-      
-      page.once('request', (req) => {
-        req.continue({
-          method: 'GET',
-          headers: {
-            ...req.headers,
-            'X-XSRF-TOKEN': oldXsrfToken,
-          },
+
+      if (oldXsrfToken) {
+        page.once('request', (req) => {
+          req.continue({
+            method: 'GET',
+            headers: {
+              ...req.headers,
+              'X-XSRF-TOKEN': oldXsrfToken.value,
+            },
+          });
         });
-      });
-      await page.setRequestInterception(true);
+      }
+      await page.setRequestInterception(Boolean(oldXsrfToken));
         try {
           await page.goto('https://www.epicgames.com/id/api/csrf');
         } catch (e) {}
       await page.setRequestInterception(false);
-      
+
 
       const xsrfToken = (await page.cookies()).find((c) => c.name === 'XSRF-TOKEN').value;
       page.once('request', (req) => {
@@ -76,6 +85,24 @@ class EpicGamesClientLoginAdapter {
     }
   }
 
+  static async authenticate(credentials, page, options) {
+    const login = credentials.login || credentials.email || credentials.username;
+    if (login && credentials.password) {
+      const loginWithEpicButton = await page.waitForSelector('#login-with-epic');
+      await loginWithEpicButton.click();
+      const usernameOrEmailField = await page.waitForSelector('#email');
+      await usernameOrEmailField.type(login, { delay: options.inputDelay });
+      const passwordField = await page.waitForSelector('#password');
+      await passwordField.type(credentials.password, { delay: options.inputDelay });
+      const loginButton = await page.waitForSelector('#login:not(:disabled)');
+      await loginButton.click();
+    }
+
+    await page.waitForResponse(this.ACCOUNT_PAGE, {
+      timeout: options.enterCredentialsTimeout,
+    });
+  }
+
   static async init (credentials={}, userOptions={}) {
     const options = {
       language: 'en-US',
@@ -99,26 +126,25 @@ class EpicGamesClientLoginAdapter {
       ],
       ...options.puppeteer,
     });
+
     const page = await browser.pages().then(pages => pages[0]);
     if (options.cookies && options.cookies.length) {
       await page.setCookie(...options.cookies);
     }
 
+    const pendingXHR = new PendingXHR(page);
+    const pendingDocument = new PendingXHR(page);
+    pendingDocument.resourceType = 'document';
     await page.goto('https://epicgames.com/id');
-    const login = credentials.login || credentials.email || credentials.username;
-    if (login && credentials.password) {
-      const loginWithEpicButton = await page.waitForSelector('#login-with-epic');
-      await loginWithEpicButton.click();
-      const usernameOrEmailField = await page.waitForSelector('#email');
-      await usernameOrEmailField.type(login, { delay: options.inputDelay });
-      const passwordField = await page.waitForSelector('#password');
-      await passwordField.type(credentials.password, { delay: options.inputDelay });
-      const loginButton = await page.waitForSelector('#login:not(:disabled)');
-      await loginButton.click();
+    await page.waitFor(1000);
+    await pendingXHR.waitOnceForAllXhrFinished();
+    await page.waitFor(1000);
+    await pendingDocument.waitOnceForAllXhrFinished();
+
+    if (page.url() != this.ACCOUNT_PAGE) {
+      await this.authenticate(credentials, page, options);
     }
-    await page.waitForResponse((response) => response.url() === 'https://www.epicgames.com/account/personal', {
-      timeout: options.enterCredentialsTimeout,
-    });
+
     return new this(browser);
   }
 
